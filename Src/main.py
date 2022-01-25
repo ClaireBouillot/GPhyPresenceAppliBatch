117be198c4cc43b854d76be40a3b0e0d4b3a76f2
rachel-villard <rachel.villard@etu.univ-poitiers.fr>
2022-01-25 14:07:29 +0100

    reception de message ok !

--- Src/main.py
+++ Src/main.py
@@ -1,56 +1,20 @@
-#!/usr/bin/env python
-"""
-A simple test server that returns a random number when sent the text "temp" via Bluetooth serial.
-"""
+import socket
 
-import os
-import glob
-import time
-import random
-
-from bluetooth import *
-
-server_sock = BluetoothSocket(RFCOMM)
-server_sock.bind(("e4:70:b8:09:df:ed", PORT_ANY))
-server_sock.listen(1)
-
-port = server_sock.getsockname()[1]
-
-uuid = "4d36e972-e325-11ce-bfc1-08002be10318"
-
-advertise_service(server_sock, "TestServer", service_id=uuid, service_classes=[uuid, SERIAL_PORT_CLASS], profiles=[SERIAL_PORT_PROFILE]) #protocols=[OBEX_UUID])
-
-print("Waiting for connection on RFCOMM chanel %d" % port)
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
-
-while True:
-
-    try:
-        req = client_sock.recv(1024)
-        if len(req) == 0:
-            break
-        print("received [%s]" % req)
-
-        data = None
-        if req in ('temp', '*temp'):
-            data = str(random.random())+'!'
-        else:
-            pass
-
+hostMACAddress = 'e4:70:B8:09:df:ed' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 5 # 3 is an arbitrary choice. However, it must match the port used by the client.
+backlog = 1
+size = 1024
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.bind((hostMACAddress,port))
+s.listen(backlog)
+try:
+    client, address = s.accept()
+    while 1:
+        data = client.recv(size)
         if data:
-            print("sending [%s]" % data)
-            client_sock.send(data)
-
-    except IOError:
-        pass
-
-    except KeyboardInterrupt:
-
-        print("disconnected")
-
-        client_sock.close()
-        server_sock.close()
-        print("all done")
-
-        break
+            print(data)
+            client.send(data)
+except:
+    print("Closing socket")
+    client.close()
+    s.close()
