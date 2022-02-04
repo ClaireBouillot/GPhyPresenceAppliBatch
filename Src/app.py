929d7daf09ff247556c10a98adfa54f9de6c6421
rachel-villard <rachel.villard@etu.univ-poitiers.fr>
2022-02-04 10:22:35 +0100

    essais pour windows service

--- Src/app.py
+++ Src/app.py
@@ -3,11 +3,12 @@
 # Définition d'un serveur réseau gérant un système de CHAT simplifié.
 # Utilise les threads pour gérer les connexions clientes en parallèle.
 
-HOST = '04:42:1a:3f:5c:27' #adaptateur: '04:42:1a:3f:5c:27''e4:70:B8:09:df:ed'
+HOST = '04:42:1a:3f:5c:27' #adaptateur: '04:42:1a:3f:5c:27' ordinateur:'e4:70:B8:09:df:ed'
 PORT = 5
 import socket, sys, threading
 from datetime import datetime
-import mysql.connector
+import mysql
+from mysql import connector
 import time
 
 class ThreadClient(threading.Thread):
@@ -24,15 +25,14 @@ class ThreadClient(threading.Thread):
             if msgClient:
                 message=msgClient.decode("utf-8")
                 x = message.split(";")             #récupération du numéro étudiant et de l'identifiant du téléphone dans deux variable
-                print (message)
 
                 #Récupération de la date et l'heure de réception du message
                 date_recept = datetime.now()
                 formatted_date = date_recept.strftime('%Y-%m-%d %H:%M:%S'+';')
-                nom_e=""
-                prenom_e=""
-                envoi_nom=""
-                #print(formatted_date)
+                #Initialisation de variables
+                nom_e="" #Nom de l'étudiant
+                prenom_e="" #Prenom de l'étudiant
+                envoi_nom="" #Variable pour connaitre le cas pour le message retour vers l'application
 
                 #Connexion et envois des infos à la base de données
                 db = mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence")#user="gphypresence",password="kjnfkvn-456123-ibsfdbv"
@@ -40,30 +40,26 @@ class ThreadClient(threading.Thread):
                     with db.cursor() as c:
                         c.execute("select count(*) from identite where num_etu=%s ", (x[0],)) #Vérif si le numéro étudiant existe dans la table identite
                         resultat = c.fetchall()
-                        #print(resultat)
                         if resultat[0] == (0,): #Si le numéro étudiant n'existe pas dans la table identite
                             c.execute("select count(*) from liste_etu where num_etu=%s ", (x[0],))
                             resultat2 = c.fetchall()
                             if resultat2[0] == (1,): #Si le numéro est dans la liste des étudiants
-                                c.execute("select nom_etu from liste_etu where num_etu=%s ", (x[0],))
+                                c.execute("select nom_etu from liste_etu where num_etu=%s ", (x[0],)) #Sélection du nom de l'étudiant dans la liste de la bdd
                                 nom_e=c.fetchall()
-                                #print(nom_e)
-                                c.execute("select prenom_etu from liste_etu where num_etu=%s ", (x[0],))
+                                c.execute("select prenom_etu from liste_etu where num_etu=%s ", (x[0],)) #Sélection du prénom de l'étudiant dans la liste de la bdd
                                 prenom_e=c.fetchall()
-                                #print(prenom_e)
-                                #if nom_e[0] != "" and prenom_e[0] != "":
+                                #Cas 1 : Si le numéro étudiant est dans la liste des étudiants
                                 envoi_nom="ok"
-                                #else:
-                                    #envoi_nom="pas ok"
-                            else: #Si le numéro n'est pas dans la liste des étudiants
+                            else:
+                                #Cas 2: Si le numéro étudiant n'est pas dans la liste des étudiants
                                 envoi_nom="pas ok"
-                            #Si le numéro etudiant n'existe pas dans identité
+                        #Si le numéro etudiant n'existe pas dans la table identité
                             try:
                                 #print("ajout...")
                                 c.execute("INSERT INTO Identite ( num_etu, num_IMEI)  VALUES(%s, %s)", x) #Insert l'étudiant dans la base si il n'y est pas déjà
                                 db.commit()
-                                #c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
-                                #db.commit()
+                                c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
+                                db.commit()
                             except mysql.connector.errors.IntegrityError:
                                 print("Etudiant déjà inscrit dans la base de données")
                         else:
@@ -73,24 +69,17 @@ class ThreadClient(threading.Thread):
                             if id_tel[0] != (0,):
                                 c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
                                 db.commit()
-                            else:
-                                envoi_nom="mauvais identifiant telephone"
                 db.close()
 
-
-                if envoi_nom == "ok" :
+                #Envoi des messages retour
+                if envoi_nom == "ok" : #Première connexion avec le numéro entré dans l'appli qui correspond à une identité dans la liste
                     nom_prenom="".join(nom_e[0]) + " " + "".join(prenom_e[0]) + ";"
-                    #print(nom_prenom)
                     self.connexion.send(nom_prenom.encode())
-                    #newmsg=self.connexion.recv(1024)
-                    #if newmsg:
-                    #    new_message=newmsg.decode("utf-8")
-                    #    print(new_message)
-                elif envoi_nom == "pas ok":
-                    self.connexion.send("Numéro étudiant inconnu;".encode())
-                elif envoi_nom == "mauvais identifiant telephone":
+                elif envoi_nom == "pas ok": #Première connexion avec le numéro entré dans l'appli qui ne correspond pas à une identité dans la liste
+                    self.connexion.send("Numéro étudiant inconnu".encode())
+                elif envoi_nom == "mauvais identifiant telephone": #Check avec changement de telephone : l'identifiant du téléphone n'est pas le même que celui dans la bdd
                     self.connexion.send("Mauvais identifiant de téléphone;".encode())
-                else:
+                else: #Check sans problème
                     date_retour=formatted_date.encode("utf-8")
                     print(date_retour)
                     self.connexion.send(date_retour)
@@ -102,35 +91,31 @@ class ThreadClient(threading.Thread):
         # Le thread se termine ici
         return true;
 
-
-# Initialisation du serveur - Mise en place du socket :
-mySocket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-try:
-   mySocket.bind((HOST, PORT))
-except socket.error:
-   print ("La liaison du socket à l'adresse choisie a échoué.")
-   sys.exit()
-print ("Serveur prêt, en attente de requêtes ...")
-mySocket.listen(1)
-
-# Attente et prise en charge des connexions demandées par les clients :
-conn_client = {}                # dictionnaire des connexions clients
-while 1:
+def run_main(): #Programme principal
+    # Initialisation du serveur - Mise en place du socket :
+    mySocket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
     try:
-        connexion, adresse = mySocket.accept()
-    # Créer un nouvel objet thread pour gérer la connexion :
-        th = ThreadClient(connexion)
-        th.start()
-    # Mémoriser la connexion dans le dictionnaire :
-        it = th.getName()        # identifiant du thread
-        conn_client[it] = connexion
-        print ("Client %s connecté, adresse IP %s, port %s." % \
-            (it, adresse[0], adresse[1]))
-    # Dialogue avec le client :
-        #date_recept = datetime.now();
-        #formatted_date = date_recept.strftime('%Y-%m-%d %H:%M:%S'+';');
-        #date_retour=formatted_date.encode("utf-8");
-        #connexion.send(date_retour);
-        #connexion.send(date_retour).encode())
-    except:
-        mySocket.close()
+        mySocket.bind((HOST, PORT))
+    except socket.error:
+        print ("La liaison du socket à l'adresse choisie a échoué.")
+        sys.exit()
+    print ("Serveur prêt, en attente de requêtes ...")
+    mySocket.listen(1)
+
+    # Attente et prise en charge des connexions demandées par les clients :
+    conn_client = {}                # dictionnaire des connexions clients
+    while 1:
+        try:
+            connexion, adresse = mySocket.accept()
+            # Créer un nouvel objet thread pour gérer la connexion :
+            th = ThreadClient(connexion)
+            th.start()
+            # Mémoriser la connexion dans le dictionnaire :
+            it = th.getName()        # identifiant du thread
+            conn_client[it] = connexion
+            print ("Client %s connecté, adresse IP %s, port %s." % \
+                   (it, adresse[0], adresse[1]))
+        except:
+            mySocket.close()
+
+##run_main()
--- Src/main.py
+++ Src/main.py
@@ -5,94 +5,111 @@ from datetime import datetime
 import mysql.connector
 import time
 
-#Bluetooth receiving message
-hostMACAddress = 'e4:70:B8:09:df:ed'#'e4:42:1a:3f:5c:27' #            #The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-#hostname=socket.gethostname()
-port = 5 # 3 is an arbitrary choice. However, it must match the port used by the client.
-backlog = 1
-size = 1024
-
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
-s.listen(backlog)
-
-ok=0
-while ok==0:
-    try:
-        print("En écoute...")
-        time.sleep(20)
-        client, address = s.accept()
-        ok=1
-        while 1:
-            data = client.recv(size)
-            if data:
-                #print(data)
-                #Récupération des deux numéros du message
-                new_data = data.decode("utf-8")     #data:bytes --> Il faut décoder les bytes en string
-                x = new_data.split(";")             #récupération du numéro étudiant et de l'identifiant du téléphone dans deux variable
-                print(x[0], x[1])                   #x[0]: n° étudiant ; x[1]:n°téléphone
-
-                #Récupération de la date et l'heure de réception du message
-                date_recept = datetime.now()
-                formatted_date = date_recept.strftime('%Y-%m-%d %H:%M:%S')
-                print(formatted_date)
-
-                #Connexion et envois des infos à la base de données
-                db = mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence")#user="gphypresence",password="kjnfkvn-456123-ibsfdbv"
-                with mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence") as db :
-                    with db.cursor() as c:
-                        c.execute("select count(*) from identite where num_etu=%s ", (x[0],)) #Vérif si le numéro étudiant existe dans la base
-                        resultat = c.fetchall()
-                        print(resultat)
-                        if resultat[0] == (0,):
-                            try:
-                                print("ajout...")
-                                c.execute("INSERT INTO Identite ( num_etu, num_IMEI)  VALUES(%s, %s)", x) #Insert l'étudiant dans la base si il n'y est pas déjà
-                                c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
-                                db.commit()
-                            except mysql.connector.errors.IntegrityError:
-                                print("Etudiant déjà inscrit dans la base de données")
-                        else:
-                            c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
-                            db.commit()
-                db.close()
-
-                #Générer la date et l'heure à laquelle le message a été reçu et l'envoyer à l'app
-                date_retour = formatted_date.encode("utf-8")
-                print(date_retour)
-                client.send(date_retour)
-                client.close()
-                s.close()
-                ok=0
-    except:
-        print("Closing socket")
-        client.close()
-        s.close()
+# #Bluetooth receiving message
+# hostMACAddress = 'e4:70:B8:09:df:ed'#'e4:42:1a:3f:5c:27' #            #The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+# #hostname=socket.gethostname()
+# port = 5 # 3 is an arbitrary choice. However, it must match the port used by the client.
+# backlog = 1
+# size = 1024
+#
+# s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+# s.bind((hostMACAddress,port))
+# s.listen(backlog)
+#
+# ok=0
+# while ok==0:
+#     try:
+#         print("En écoute...")
+#         time.sleep(20)
+#         client, address = s.accept()
+#         ok=1
+#         while 1:
+#             data = client.recv(size)
+#             if data:
+#                 #print(data)
+#                 #Récupération des deux numéros du message
+#                 new_data = data.decode("utf-8")     #data:bytes --> Il faut décoder les bytes en string
+#                 x = new_data.split(";")             #récupération du numéro étudiant et de l'identifiant du téléphone dans deux variable
+#                 print(x[0], x[1])                   #x[0]: n° étudiant ; x[1]:n°téléphone
+#
+#                 #Récupération de la date et l'heure de réception du message
+#                 date_recept = datetime.now()
+#                 formatted_date = date_recept.strftime('%Y-%m-%d %H:%M:%S')
+#                 print(formatted_date)
+#
+#                 #Connexion et envois des infos à la base de données
+#                 db = mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence")#user="gphypresence",password="kjnfkvn-456123-ibsfdbv"
+#                 with mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence") as db :
+#                     with db.cursor() as c:
+#                         c.execute("select count(*) from identite where num_etu=%s ", (x[0],)) #Vérif si le numéro étudiant existe dans la base
+#                         resultat = c.fetchall()
+#                         print(resultat)
+#                         if resultat[0] == (0,):
+#                             try:
+#                                 print("ajout...")
+#                                 c.execute("INSERT INTO Identite ( num_etu, num_IMEI)  VALUES(%s, %s)", x) #Insert l'étudiant dans la base si il n'y est pas déjà
+#                                 c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
+#                                 db.commit()
+#                             except mysql.connector.errors.IntegrityError:
+#                                 print("Etudiant déjà inscrit dans la base de données")
+#                         else:
+#                             c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date)) #Insert le tag horaire de présence dans la base
+#                             db.commit()
+#                 db.close()
+#
+#                 #Générer la date et l'heure à laquelle le message a été reçu et l'envoyer à l'app
+#                 date_retour = formatted_date.encode("utf-8")
+#                 print(date_retour)
+#                 client.send(date_retour)
+#                 client.close()
+#                 s.close()
+#                 ok=0
+#     except:
+#         print("Closing socket")
+#         client.close()
+#         s.close()
         #ok=0
 
-
+new_data="21911504;5727972890"
+date_recept = datetime.now()
+formatted_date = date_recept.strftime('%Y-%m-%d %H:%M:%S')
 
 #Récupération des deux numéros
-# x = new_data.split(";")
-# user = (x[0], x[1])
-# etu=x[0]
+x = new_data.split(";")
+user = (x[0], x[1])
+etu=x[0]
 
-# print(formatted_date)
-# #print(x[0], x[1]) #x[0]: n° étudiant ; x[1]:n°téléphone
-#
-# #Connexion et envois des infos à la base de données
-# db = mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence")#user="gphypresence",password="kjnfkvn-456123-ibsfdbv"
-# with mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence") as db :
-#     with db.cursor() as c:
-#         c.execute("select num_etu from identite where num_etu=%s ", (x[0],))
-#         resultat = c.fetchall()
-#         if resultat[0] == '':
-#             try:
-#                 c.execute("INSERT INTO Identite ( num_etu, num_IMEI)  VALUES(%s, %s)", user)
-#                 db.commit()
-#             except mysql.connector.errors.IntegrityError:
-#                 print("Etudiant déjà inscrit dans la base de données")
-#         else:
-#             c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date))
-#             db.commit()
-# db.close()
+
+#print(x[0], x[1]) #x[0]: n° étudiant ; x[1]:n°téléphone
+
+#Connexion et envois des infos à la base de données
+db = mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence")#user="gphypresence",password="kjnfkvn-456123-ibsfdbv"
+with mysql.connector.connect(host="localhost", user="root", password="", database="gphy_presence") as db :
+    with db.cursor() as c:
+        c.execute("select count(*) from identite where num_etu=%s ", (x[0],))
+        resultat = c.fetchall()
+        print(resultat)
+        if resultat[0] == (0,):
+            c.execute("select count(*) from liste_etu where num_etu=%s ", (x[0],))
+            resultat2 = c.fetchall()
+            if resultat2[0] == (1,):
+                c.execute("select nom_etu from liste_etu where num_etu=%s ", (x[0],))
+                nom=c.fetchall()
+                print(nom)
+                c.execute("select prenom_etu from liste_etu where num_etu=%s ", (x[0],))
+                prenom=c.fetchall()
+                print(prenom)
+                envoi_nom="ok"
+            else:
+                try:
+                    c.execute("INSERT INTO identite (num_etu, num_IMEI)  VALUES(%s, %s)", user)
+                    c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date))
+                    db.commit()
+                except mysql.connector.errors.IntegrityError:
+                    print("Etudiant déjà inscrit dans la base de données")
+        else:
+            print("ok")
+            print(x[0], formatted_date)
+            c.execute("INSERT INTO presence (num_capteur, num_ident, tag_horaire)  VALUES( %s, %s, %s)", (1, x[0], formatted_date))
+            db.commit()
+db.close()
